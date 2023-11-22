5a64dfe043d29a50e76c13bc938e4d388e0a9952
ArieL19999 <125905052+ArieL19999@users.noreply.github.com>
2023-11-22 15:43:07 +0200

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -15,9 +15,15 @@ sudo su - ec2-user -c "minikube start  2>&1 | tee SomeFile.txt"
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
+# Clone the 'job-project' repository from GitHub
 git clone https://github.com/ArieL19999/job-project.git
+# Apply the Kubernetes deployment configuration from the 'deployment.yml' file
 sudo su - ec2-user -c "kubectl apply -f /job-project/deployment.yml"
+# Apply the Kubernetes service configuration from the 'service.yml' file
 sudo su - ec2-user -c "kubectl apply -f /job-project/service.yml"
+# Defining the path to the SSH key for Minikube
 SSH_KEY_PATH="/home/ec2-user/.minikube/machines/minikube/id_rsa"
+# the IP address of the Minikube machine
 MINIKUBE_IP=$(sudo su - ec2-user -c "minikube ip")
+# an SSH tunnel to forward traffic from the local machine to the Minikube instance
 sudo ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_PATH" docker@"$MINIKUBE_IP" -L *:80:0.0.0.0:32321 -N &
