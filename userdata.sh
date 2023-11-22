ea8da4867d98f7d7c99903cd116a9a0adbd0976d
ArieL19999 <ariell12340@gmail.com>
2023-11-22 12:58:05 +0200

    fifth

--- userdata.sh
+++ userdata.sh
@@ -2,25 +2,22 @@
 # Update System
 sudo yum update
 # Install Docker
-sudo yum install docker -y
+sudo yum install docker git -y
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
-sudo yum install git
 # Install Minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo chmod +x minikube
 sudo mv minikube /usr/local/bin/
 # Start Minikube
-minikube start  2>&1 | tee SomeFile.txt
-# Download kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/darwin/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+sudo su - ec2-user -c "minikube start  2>&1 | tee SomeFile.txt"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 git clone https://github.com/ArieL19999/job-project.git
-cd  job-project
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
-sudo ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -L \*:80:0.0.0.0:32321
-
-
+sudo su - ec2-user -c "kubectl apply -f /job-project/deployment.yml"
+sudo su - ec2-user -c "kubectl apply -f /job-project/service.yml"
+SSH_KEY_PATH="/home/ec2-user/.minikube/machines/minikube/id_rsa"
+MINIKUBE_IP=$(sudo su - ec2-user -c "minikube ip")
+sudo ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_PATH" docker@"$MINIKUBE_IP" -L *:80:0.0.0.0:32321 -N &
